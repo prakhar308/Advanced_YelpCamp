5cf0241925dd1453e7f0c8004e8fe40cf9a91568
prakhar308 <khandelwalprakhar30.com>
2020-02-04 14:39:05 +0530

    update campground model by adding reviews and rating fields

--- models/campground.js
+++ models/campground.js
@@ -18,7 +18,17 @@ var campgroundSchema = mongoose.Schema({
             type : mongoose.Schema.Types.ObjectId,
             ref : "Comment"
         }
-    ]
+    ],
+    reviews : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ],
+    rating : {
+        type : Number,
+        default : 0
+    }
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
