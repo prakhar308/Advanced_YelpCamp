5ecf9073299622e61cd7c2e005040c4768686717
prakhar308 <khandelwalprakhar30.com>
2020-02-05 23:42:01 +0530

    create middleware for checking review ownership and review existence

--- middleware/index.js
+++ middleware/index.js
@@ -51,6 +51,48 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
 	}
 }
 
+// middleware for review authorization
+middlewareObj.checkReviewOwnership = function(req,res,next){
+	// is user loggedIn
+	if(req.isAuthenticated()){
+		Review.findById(req.params.review_id,function(err,foundReview){
+			if(err) res.redirect("back");
+			else{
+				// does this user own the review
+				if(foundReview.author.id.equals(req.user._id))
+					next();
+				else{
+					req.flash("error","You don't have permission to that");
+					res.redirect("back");
+				}
+			}
+		})
+	} else {
+		req.flash("error","You need to be logged in to do that");
+		res.redirect("back");
+	}
+}
+
+// middleware for review existence
+middlewareObj.checkReviewExistence = function(req,res,next){
+	Campground.findById(req.params.id).populate("reviews").exec(function(err,foundCampground){
+		if(err){
+			req.flash("error", "Campground not found.");
+			res.redirect("back");
+		} else {
+			// check if current user id matches with any of the review's author id
+			var foundUserReview = foundCampground.reviews.some(function(review){
+				return review.author.id.equals(req.user._id);
+			})
+			if(foundCampground){
+				req.flash("error", "You already wrote a review");
+                return res.redirect("/campgrounds/"+foundCampground._id);
+			}
+			next();
+		}		
+	})
+}
+
 //middleware for Authentication
 middlewareObj.isLoggedIn = function(req, res, next){
 	if(req.isAuthenticated()) return next();
